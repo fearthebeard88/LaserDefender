6697a89eb5cceaedbb1d51f7d5f8e48924c1fc22
Tim <tferris88@gmail.com>
2018-11-02 20:50:15 -0400

    enemy moves, created wave config set up

---
+++ LaserDefender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    [SerializeField] public List<Transform> waypoints;
+    [SerializeField] public float moveSpeed = 5f;
+    public int waypointIndex = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    transform.position = this.waypoints[this.waypointIndex].transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (this.waypointIndex <= this.waypoints.Count - 1)
+        {
+            var targetPosition = this.waypoints[this.waypointIndex].transform.position;
+            var movementThisFrame = this.moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            if (transform.position == targetPosition)
+            {
+                this.waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ LaserDefender/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] public GameObject enemyPrefab;
+    [SerializeField] public GameObject pathPrefab;
+    [SerializeField] public float timeBetweenSpawns = 0.5f;
+    [SerializeField] public float spawnRandomFactor = 0.3f;
+    [SerializeField] public int numberOfEnemies = 5;
+    [SerializeField] public float moveSpeed = 2f;
+
+    public GameObject GetEnemyPrefab() { return this.enemyPrefab;}
+    public GameObject GetPathPrefab() { return this.pathPrefab;}
+    public float GetTimeBetweenSpawns() { return this.timeBetweenSpawns;}
+    public int GetNumberOfEnemies() { return this.numberOfEnemies;}
+    public float GetMoveSpeed() { return this.moveSpeed;}
+}
